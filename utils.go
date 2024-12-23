1ac5f823a71f577ddffa83d7f147244cd10710fb
Fardjad Davari <public@fardjad.com>
2024-12-23 23:46:22 +0000

    Add learning tests

---
+++ utils.go
@@ -0,0 +1,21 @@
+package main
+
+import "net"
+
+func Must[T any](obj T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return obj
+}
+
+func FindFreePort() int {
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	defer listener.Close()
+
+	return listener.Addr().(*net.TCPAddr).Port
+}
